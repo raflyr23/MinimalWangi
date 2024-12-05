91a0418c1f46a1b7011215411106e550849dd918
raflyr23 <gheadarling@gmail.com>
2024-12-05 11:27:24 +0700

    fixx

--- resources/views/frontend/contact/header.blade.php
+++ resources/views/frontend/contact/header.blade.php
@@ -1,67 +1,81 @@
 <nav class="navbar navbar-expand-lg navbar-light shadow">
-    <div class="container d-flex justify-content-between align-items-center">
-        <a class="navbar-brand align-self-center" href="{{ url('/') }}">
+    <div class="container">
+        <!-- Logo -->
+        <a class="navbar-brand" href="{{ url('/') }}">
             <img src="./assets/img/logo.png" alt="MinimalWangi Logo" class="logo">
         </a>
-        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
-            <div class="flex-fill">
-                <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/shop') }}">Shop</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/contact') }}">Contact</a></li>
-                </ul>
-            </div>
-            <div class="navbar align-self-center d-flex">
-                <div class="d-lg-none flex-sm-fill mt-3 mb-4 col-7 col-sm-auto pr-3">
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="inputMobileSearch" placeholder="Search ...">
-                        <div class="input-group-text"><i class="fa fa-fw fa-search"></i></div>
-                    </div>
+        <!-- Cart and User icons for mobile -->
+        <div class="d-flex d-lg-none">
+            <a class="nav-icon position-relative text-decoration-none" href="{{ url('show_cart') }}">
+                <i class="fa fa-fw fa-cart-arrow-down text-dark"></i>
+                @if($cart_count > 0)
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        {{ $cart_count }}
+                    </span>
+                @endif
+            </a>
+            
+            <!-- Toggler -->
+            <button class="navbar-toggler border-0 ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+
+        <!-- Collapsible content -->
+        <div class="collapse navbar-collapse" id="templatemo_main_nav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/shop') }}">Shop</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/contact') }}">Contact</a></li>
+            </ul>
+
+            <!-- Search form -->
+            <form action="{{ url('product_search') }}" method="GET" class="d-flex mb-2 mb-lg-0" id="search-form">
+                @csrf
+                <div class="input-group">
+                    <input type="text" name="search" class="form-control" placeholder="Search ...">
+                    <button type="submit" class="btn btn-light">
+                        <i class="fa fa-search"></i>
+                    </button>
                 </div>
-                <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal" data-bs-target="#templatemo_search">
-                    <i class="fa fa-fw fa-search text-dark mr-2"></i>
-                </a>
+            </form>
+
+            <!-- Cart and User icons for desktop -->
+            <div class="d-none d-lg-flex align-items-center">
                 <a class="nav-icon position-relative text-decoration-none" href="{{ url('show_cart') }}">
-                    <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
+                    <i class="fa fa-fw fa-cart-arrow-down text-dark"></i>
                     @if($cart_count > 0)
                         <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                             {{ $cart_count }}
                         </span>
                     @endif
                 </a>
-                
 
-                <div class="dropdown">
-    <a class="nav-icon position-relative text-decoration-none dropdown-toggle" href="#" role="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-        <i class="fa fa-fw fa-user text-dark mr-3"></i>
-    </a>
-    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-        @if (Route::has('login'))
-            @auth
-                <li><a class="dropdown-item" href="dashboard">User</a></li>
-                <li><a class="dropdown-item" href="{{ route('order.index') }}">Order</a></li>
-                <li>
-                    <a href="#" class="dropdown-item" 
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        Logout
+                <!-- User Dropdown -->
+                <div class="dropdown ms-3">
+                    <a class="nav-icon position-relative text-decoration-none dropdown-toggle" href="#" role="button" id="userDropdown" data-bs-toggle="dropdown">
+                        <i class="fa fa-fw fa-user text-dark"></i>
                     </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </li>
-            @else
-                <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
-                <li><a class="dropdown-item" href="{{ route('register') }}">Register</a></li>
-            @endauth
-        @endif
-    </ul>
-</div>
-
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        @if (Route::has('login'))
+                            @auth
+                                <li><a class="dropdown-item" href="dashboard">User</a></li>
+                                <li><a class="dropdown-item" href="{{ route('order.index') }}">Order</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li>
+                                    <a href="#" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+                                </li>
+                            @else
+                                <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+                                <li><a class="dropdown-item" href="{{ route('register') }}">Register</a></li>
+                            @endauth
+                        @endif
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/frontend/shop/header.blade.php
+++ resources/views/frontend/shop/header.blade.php
@@ -1,43 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-light shadow">
-    <div class="container d-flex justify-content-between align-items-center">
+    <div class="container">
         <!-- Logo -->
-        <a class="navbar-brand align-self-center" href="{{ url('/') }}">
+        <a class="navbar-brand" href="{{ url('/') }}">
             <img src="./assets/img/logo.png" alt="MinimalWangi Logo" class="logo">
         </a>
-        
-        <!-- Toggler untuk navbar di mobile -->
-        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <!-- Menu Navbar -->
-        <div class="align-self-center collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
-            <div class="flex-fill">
-                <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/shop') }}">Shop</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ url('/contact') }}">Contact</a></li>
-                </ul>
-            </div>
-
-            <!-- Formulir Pencarian -->
-            <form action="{{ url('product_search') }}" method="GET" class="d-flex align-items-center" id="search-form">
-                @csrf
-                <!-- Tombol Pencarian (Ikon Search) -->
-                <button type="button" class="btn btn-light me-1" id="search-icon">
-                    <i class="fa fa-fw fa-search"></i>
-                </button>
-
-                <!-- Input Pencarian (Disembunyikan pada awalnya) -->
-                <input type="text" name="search" class="form-control" id="inputMobileSearch" placeholder="Search ..." style="display: none; width: 200px; border-radius: 20px; transition: width 0.4s ease;">
-                
-                <!-- Tombol Submit Pencarian (Disembunyikan) -->
-                <button type="submit" style="display: none;" id="submit-search"></button>
-            </form>
-
-            <!-- Keranjang Belanja -->
-            <a class="nav-icon position-relative text-decoration-none ms-1" href="{{ url('show_cart') }}">
+        <!-- Cart and User icons for mobile -->
+        <div class="d-flex d-lg-none">
+            <a class="nav-icon position-relative text-decoration-none" href="{{ url('show_cart') }}">
                 <i class="fa fa-fw fa-cart-arrow-down text-dark"></i>
                 @if($cart_count > 0)
                     <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
@@ -45,56 +15,68 @@
                     </span>
                 @endif
             </a>
+            
+            <!-- Toggler -->
+            <button class="navbar-toggler border-0 ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
 
-            <!-- Dropdown User -->
-            <div class="dropdown">
-                <a class="nav-icon position-relative text-decoration-none dropdown-toggle ms-3" href="#" role="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fa fa-fw fa-user text-dark"></i>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                    @if (Route::has('login'))
-                        @auth
-                            <li><a class="dropdown-item" href="dashboard">User</a></li>
-                            <li><a class="dropdown-item" href="{{ route('order.index') }}">Order</a></li>
-                            <li>
-                                <a href="#" class="dropdown-item" 
-                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </li>
-                        @else
-                            <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="dropdown-item" href="{{ route('register') }}">Register</a></li>
-                        @endauth
+        <!-- Collapsible content -->
+        <div class="collapse navbar-collapse" id="templatemo_main_nav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/shop') }}">Shop</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('/contact') }}">Contact</a></li>
+            </ul>
+
+            <!-- Search form -->
+            <form action="{{ url('product_search') }}" method="GET" class="d-flex mb-2 mb-lg-0" id="search-form">
+                @csrf
+                <div class="input-group">
+                    <input type="text" name="search" class="form-control" placeholder="Search ...">
+                    <button type="submit" class="btn btn-light">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </form>
+
+            <!-- Cart and User icons for desktop -->
+            <div class="d-none d-lg-flex align-items-center">
+                <a class="nav-icon position-relative text-decoration-none" href="{{ url('show_cart') }}">
+                    <i class="fa fa-fw fa-cart-arrow-down text-dark"></i>
+                    @if($cart_count > 0)
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                            {{ $cart_count }}
+                        </span>
                     @endif
-                </ul>
+                </a>
+
+                <!-- User Dropdown -->
+                <div class="dropdown ms-3">
+                    <a class="nav-icon position-relative text-decoration-none dropdown-toggle" href="#" role="button" id="userDropdown" data-bs-toggle="dropdown">
+                        <i class="fa fa-fw fa-user text-dark"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        @if (Route::has('login'))
+                            @auth
+                                <li><a class="dropdown-item" href="dashboard">User</a></li>
+                                <li><a class="dropdown-item" href="{{ route('order.index') }}">Order</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li>
+                                    <a href="#" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+                                </li>
+                            @else
+                                <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+                                <li><a class="dropdown-item" href="{{ route('register') }}">Register</a></li>
+                            @endauth
+                        @endif
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
 </nav>
-
-<!-- Script untuk menampilkan input pencarian saat ikon search diklik -->
-<script>
-    const searchIcon = document.getElementById('search-icon');
-    const searchInput = document.getElementById('inputMobileSearch');
-    const submitSearch = document.getElementById('submit-search');
-
-    searchIcon.addEventListener('click', function() {
-        // Cek apakah input pencarian sedang disembunyikan atau tidak
-        if (searchInput.style.display === 'none' || searchInput.style.display === '') {
-            // Tampilkan input pencarian dan tombol submit
-            searchInput.style.display = 'block';
-            searchInput.style.width = '250px'; // Lebar input berubah ketika terlihat
-            submitSearch.style.display = 'inline-block';
-            searchInput.focus();
-        } else {
-            // Sembunyikan input pencarian dan tombol submit jika sudah terlihat
-            searchInput.style.display = 'none';
-            submitSearch.style.display = 'none';
-            searchInput.style.width = '200px'; // Mengembalikan lebar input ke semula
-        }
-    });
-</script>
